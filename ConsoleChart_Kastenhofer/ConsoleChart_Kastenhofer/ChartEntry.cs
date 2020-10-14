1cbba46be1e2e6ddf0901fde7e517632cdab51f5
Daniel <daniel.kastenhofer1@gmail.com>
2020-10-14 17:05:29 +0200

    final commmit

---
+++ ConsoleChart_Kastenhofer/ConsoleChart_Kastenhofer/ChartEntry.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleChart_Kastenhofer
+{
+    class ChartEntry
+    {
+
+        public ChartEntry(string entryTitle, int entryCount)
+        {
+            EntryTitle = entryTitle;
+            EntryCount = entryCount;
+        }
+
+        public string EntryTitle { get; }
+
+        public int EntryCount { get; }
+    }
+}
---
+++ ConsoleChart_Kastenhofer/ConsoleChart_Kastenhofer/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleChart_Kastenhofer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<ChartEntry> chartEntries = new List<ChartEntry>();
+
+            var categories = Console.ReadLine().Split('\t');
+
+            var groupingColIdx = Array.IndexOf(categories, args[0]);
+            var numericColIdx = Array.IndexOf(categories, args[1]);
+            var entryCount = Convert.ToInt32(args[2]);  
+
+
+            //Read from inputfile
+            while (true)
+            {
+                var currentLine = Console.ReadLine();
+                if (string.IsNullOrEmpty(currentLine))
+                    break;
+
+                var lineSplitted = currentLine.Split('\t');
+
+                chartEntries.Add(new ChartEntry(lineSplitted[groupingColIdx], Convert.ToInt32(lineSplitted[numericColIdx])));
+            }
+
+            //Write output
+
+            var groupedEntries = chartEntries.GroupBy(entry => entry.EntryTitle)
+                                 .Select(entry => new ChartEntry(entry.Key, entry.Sum(e => e.EntryCount)))
+                                 .OrderByDescending(entry => entry.EntryCount)
+                                 .ToList();
+
+            int highestAttackCount = groupedEntries.Max(entry => entry.EntryCount);
+            var count = 0;
+            foreach (var entry in groupedEntries)
+            {
+
+                var percentage = (int) ((double) entry.EntryCount / highestAttackCount * 100);
+                
+                Console.Write($"{entry.EntryTitle,-40} |");
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"{ GetBarForPercentage(percentage)}");
+                Console.ForegroundColor = ConsoleColor.Gray;
+
+                if (count == entryCount)
+                {
+                    break;
+                }
+                count++;
+            }
+        }
+
+        static String GetBarForPercentage(int percentage)
+        {
+            return new string('█', percentage) + new string(' ', (100 - percentage));
+        }
+    }
+}
